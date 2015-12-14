a84900f8592605f0b9f3b21cd1b12f6356702144
Tyler Martinez <slightlytyler@gmail.com>
2015-12-14 15:29:25 -0600

    Propery config for webpack to load styles

--- gatsby.config.js
+++ gatsby.config.js
@@ -2,7 +2,13 @@ export default function(config, env) {
   config.loader('fonts', function(cfg) {
     cfg.test = /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/;
     cfg.loader = 'file-loader'
-    return cfg
-  })
-  return config;
+
+    return cfg;
+  });
+
+  return env === 'production' ?
+      config.merge({
+        output: { publicPath: '/' }
+      }) :
+      config;
 }
